057c87c43edc9226c9fa465cc62006da1aca39ef
SdevWeb7 <sd@sdevweb.com>
2024-05-26 21:02:51 +0200

    Backup database

--- 3wa_services.sql
+++ 3wa_services.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 22 mai 2024 à 09:00
+-- Généré le : dim. 26 mai 2024 à 19:01
 -- Version du serveur : 8.3.0
 -- Version de PHP : 8.2.18
 
@@ -38,7 +38,49 @@ CREATE TABLE IF NOT EXISTS `messagerie` (
   PRIMARY KEY (`id`),
   KEY `from_user_id` (`from_user_id`),
   KEY `messagerie_ibfk_2` (`to_user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `messagerie`
+--
+
+INSERT INTO `messagerie` (`id`, `from_user_id`, `to_user_id`, `subject`, `content`, `created_at`) VALUES
+(4, 65, 66, 'Bonjour', 'J\'aimerais avoir plus de précisions svp', '2024-05-22 11:04:16'),
+(5, 65, 67, 'precision svp', 'votre service n est pas clair please', '2024-05-22 11:04:34'),
+(6, 65, 67, 'bonjour', 'ceci est un test', '2024-05-24 23:46:40'),
+(7, 66, 65, 'Bonjour', 'avec plaisir je vous répondrais', '2024-05-25 14:45:10');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `from_user_id` int UNSIGNED NOT NULL,
+  `service_id` int UNSIGNED NOT NULL,
+  `to_user_id` int UNSIGNED NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `start_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `from_user_id` (`from_user_id`),
+  KEY `service_id` (`service_id`),
+  KEY `to_user_id` (`to_user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `orders`
+--
+
+INSERT INTO `orders` (`id`, `from_user_id`, `service_id`, `to_user_id`, `status`, `created_at`, `start_date`) VALUES
+(25, 66, 37, 67, 'En cours', '2024-05-25 14:44:45', '2024-05-12 12:44:00'),
+(26, 65, 41, 66, 'En cours', '2024-05-25 14:45:33', '2024-05-12 12:45:00'),
+(27, 65, 38, 67, 'En cours', '2024-05-25 14:45:37', '2024-05-30 12:45:00'),
+(28, 67, 40, 66, 'En cours', '2024-05-25 14:46:10', '2024-05-18 12:46:00'),
+(29, 67, 41, 66, 'En cours', '2024-05-25 14:46:13', '2024-06-02 12:46:00');
 
 -- --------------------------------------------------------
 
@@ -55,25 +97,28 @@ CREATE TABLE IF NOT EXISTS `service` (
   `duration` int UNSIGNED NOT NULL,
   `category` varchar(50) NOT NULL,
   `created_at` datetime NOT NULL,
+  `img_src` varchar(100) NOT NULL,
   `user_id` int UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `service_ibfk_1` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `service`
 --
 
-INSERT INTO `service` (`id`, `title`, `description`, `cost`, `duration`, `category`, `created_at`, `user_id`) VALUES
-(22, 'Cours de Javascript', 'Bonjour je vous propose des cours de vanilla javascript.', 200, 2, 'Informatique', '2024-05-22 10:10:49', 65),
-(23, 'Cours de Next.js', 'Bonjour je vous propose des cours de Next.js.', 300, 2, 'Informatique', '2024-05-22 10:11:09', 65),
-(24, 'Cours de Symfony', 'Bonjour je vous propose des cours de Symfony.', 150, 2, 'Informatique', '2024-05-22 10:11:27', 65),
-(25, 'Cours de HTML/CSS', 'Bonjour je vous propose des cours de HTML/CSS.', 150, 2, 'Informatique', '2024-05-22 10:11:41', 65),
-(26, 'Taille de haies', 'Bonjour je vous propose de venir tailler vos haies.', 200, 1, 'Jardinage', '2024-05-22 10:13:15', 66),
-(27, 'Pose et coupe de gazon', 'Bonjour je vous propose de venir poser et couper votre gazon.', 200, 1, 'Jardinage', '2024-05-22 10:13:43', 66),
-(28, 'Plante d\'arbre', 'Bonjour je vous propose de venir planter un ou des arbres.', 250, 1, 'Jardinage', '2024-05-22 10:14:11', 66),
-(29, 'Pose de carrelage', 'Bonjour je propose de venir poser votre carrelage.', 300, 2, 'Bricolage', '2024-05-22 10:17:38', 67),
-(30, 'Création de meubles', 'Bonjour je propose de venir créer des meubles sur mesure.', 300, 2, 'Bricolage', '2024-05-22 10:18:03', 67);
+INSERT INTO `service` (`id`, `title`, `description`, `cost`, `duration`, `category`, `created_at`, `img_src`, `user_id`) VALUES
+(31, 'Cours de Javascript', 'J\'ai le plaisir de vous proposer des cours de javascript', 200, 2, 'Informatique', '2024-05-25 13:54:00', '', 65),
+(32, 'Cours HTML CSS', 'J\'ai le plaisir de vous proposer des cours HTML CSS', 200, 2, 'Informatique', '2024-05-25 13:54:15', '', 65),
+(33, 'Cours Nextjs', 'J\'ai le plaisir de vous proposer des cours Nextjs', 200, 2, 'Informatique', '2024-05-25 13:54:27', '', 65),
+(36, 'Pose de carrelage', 'J\'ai le plaisir de vous proposer de poser cotre carrelage.', 150, 2, 'Bricolage', '2024-05-25 13:56:12', '', 67),
+(37, 'Service de bricolage', 'Je peux briocoler pour vous ce que vous voulez', 300, 3, 'Bricolage', '2024-05-25 14:41:43', '', 67),
+(38, 'Monter meuble', 'Je peux monter vos meubles.', 100, 1, 'Bricolage', '2024-05-25 14:42:13', '', 67),
+(39, 'Tailler haies', 'Je vous propose de tailler vos haies', 150, 1, 'Jardinage', '2024-05-25 14:43:39', '', 66),
+(40, 'Planter arbre', 'Je vous propose de planter l\'arbre que vous voulez', 150, 1, 'Jardinage', '2024-05-25 14:43:52', '', 66),
+(41, 'Tondre gazon', 'Je vous propose de tondre votre gazon', 150, 1, 'Jardinage', '2024-05-25 14:44:07', '', 66),
+(43, 'Full-stack dev', 'Devenir full stack en 1h par jour :)', 200, 2, 'Informatique', '2024-05-26 17:39:19', 'fullstackDevSkills.jpg', 65),
+(48, 'Tuto ViteJS', 'Comment démarrer rapidement avec ViteJS', 200, 2, 'Informatique', '2024-05-26 18:10:44', 'vite.svg', 67);
 
 -- --------------------------------------------------------
 
@@ -94,38 +139,7 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 --
 
 INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
-('WYzBKSVK_gWDOwxOCZnSPNTKc9fbmEEz', 1716973079, '{\"cookie\":{\"originalMaxAge\":604800000,\"expires\":\"2024-05-29T08:50:11.630Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\",\"sameSite\":\"lax\"},\"user\":{\"id\":65,\"email\":\"test@test.fr\",\"sold\":1000,\"services_rendered\":0,\"created_at\":\"2024-05-22T08:08:20.000Z\"}}');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `transaction_status`
---
-
-DROP TABLE IF EXISTS `transaction_status`;
-CREATE TABLE IF NOT EXISTS `transaction_status` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `from_user_id` int UNSIGNED NOT NULL,
-  `service_id` int UNSIGNED NOT NULL,
-  `to_user_id` int UNSIGNED NOT NULL,
-  `status` varchar(20) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `start_date` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `from_user_id` (`from_user_id`),
-  KEY `service_id` (`service_id`),
-  KEY `to_user_id` (`to_user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Déchargement des données de la table `transaction_status`
---
-
-INSERT INTO `transaction_status` (`id`, `from_user_id`, `service_id`, `to_user_id`, `status`, `created_at`, `start_date`) VALUES
-(8, 67, 29, 67, 'En cours', '2024-05-22 10:30:10', '2024-05-19 08:30:00'),
-(9, 67, 22, 65, 'En cours', '2024-05-22 10:30:17', '2024-05-09 08:30:00'),
-(10, 65, 29, 67, 'En cours', '2024-05-22 10:40:20', '2024-05-12 08:40:00'),
-(11, 65, 25, 65, 'Finalisée', '2024-05-22 10:40:30', '2024-05-26 08:40:00');
+('0rxxJBTsF0DJH6s6IqNoD8SST2jPajNJ', 1717338958, '{\"cookie\":{\"originalMaxAge\":604800000,\"expires\":\"2024-06-02T14:35:58.295Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\",\"sameSite\":\"lax\"},\"user\":{\"id\":65,\"email\":\"test@test.fr\",\"sold\":850,\"services_rendered\":2,\"created_at\":\"2024-05-22T08:08:20.000Z\"}}');
 
 -- --------------------------------------------------------
 
@@ -149,9 +163,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `sold`, `services_rendered`, `created_at`) VALUES
-(65, 'test@test.fr', '$2b$10$Epw2kvlShQ2sNtgjecC6heWYuuUitgNLHMtAVzN2297ulgsQDcf7K', 1000, 0, '2024-05-22 10:08:20'),
+(65, 'test@test.fr', '$2b$10$Epw2kvlShQ2sNtgjecC6heWYuuUitgNLHMtAVzN2297ulgsQDcf7K', 850, 2, '2024-05-22 10:08:20'),
 (66, 'jardinier@plantes.fr', '$2b$10$lju4Zm3H60FzWBsifl9yIeW.8oFA5.v5F2edIyhb88Kj0vN5QRrTu', 1000, 0, '2024-05-22 10:12:16'),
-(67, 'bricoleur@brico.fr', '$2b$10$GHbUOi3fTKoRgjCZ5G9rb.pHLYSmZXd0b.JcTcTEvZFjfwCevjh7u', 1000, 0, '2024-05-22 10:16:49');
+(67, 'bricoleur@brico.fr', '$2b$10$GHbUOi3fTKoRgjCZ5G9rb.pHLYSmZXd0b.JcTcTEvZFjfwCevjh7u', 1150, 3, '2024-05-22 10:16:49');
 
 --
 -- Contraintes pour les tables déchargées
@@ -165,18 +179,18 @@ ALTER TABLE `messagerie`
   ADD CONSTRAINT `messagerie_ibfk_2` FOREIGN KEY (`to_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Contraintes pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`from_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_4` FOREIGN KEY (`to_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Contraintes pour la table `service`
 --
 ALTER TABLE `service`
   ADD CONSTRAINT `service_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `transaction_status`
---
-ALTER TABLE `transaction_status`
-  ADD CONSTRAINT `transaction_status_ibfk_1` FOREIGN KEY (`from_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_status_ibfk_3` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_status_ibfk_4` FOREIGN KEY (`to_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
